f2929653ede90551fdfd1ceacb3218f3f56493b1
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-05-07 08:38:57 +0100

    Add harmless ransom note algorithm.

---
+++ harmless-ransom-note/harmless-ransom-note.js
@@ -0,0 +1,36 @@
+function harmlessRansomNote(note, magazineText) {
+  const validInput =
+    typeof note === "string" && typeof magazineText === "string";
+  if (!validInput) return false;
+
+  const noteArray = note.split(" ").filter(Boolean);
+  const magazineArray = magazineText.split(" ").filter(Boolean);
+  const magazineHashTable = {};
+
+  if (noteArray.length < 1 && magazineArray.length < 1) {
+    return false;
+  }
+
+  magazineArray.forEach(word => {
+    if (!magazineHashTable[word]) magazineHashTable[word] = 0;
+    magazineHashTable[word]++;
+  });
+
+  let noteIsPossible = true;
+
+  noteArray.forEach(word => {
+    if (magazineHashTable[word]) {
+      magazineHashTable[word]--;
+
+      if (magazineHashTable[word] < 0) {
+        return (noteIsPossible = false);
+      }
+    } else {
+      return (noteIsPossible = false);
+    }
+  });
+
+  return noteIsPossible;
+}
+
+module.exports.default = harmlessRansomNote;
---
+++ harmless-ransom-note/tests/index.test.js
@@ -0,0 +1,29 @@
+const harmlessRansomNote = require("../harmless-ransom-note").default;
+
+describe("Harmless Ransom Note", () => {
+  test("should be able to handle invalid input", () => {
+    expect(harmlessRansomNote(1, 2)).toBeFalsy();
+    expect(harmlessRansomNote({ test: "hey" }, 121211)).toBeFalsy();
+  });
+
+  test("should be able to handle empty input", () => {
+    expect(harmlessRansomNote(" ", " ")).toBeFalsy();
+  });
+
+  test("should be able to produce a ransom note", () => {
+    const validMagazineText = `
+        puerto rico is a place of great wonder and excitement it has many secret waterfall locations,
+        that i am an admirer of you must hike quite a distance to find the secret places as they are far
+        from populated areas but it is worth the effort a tip i have for you is to go early in the morning
+        when it is not so hot out also note that you must wear hiking boots
+        this is one of the best places i have ever visited
+    `;
+
+    expect(
+      harmlessRansomNote(
+        "this is a secret note for you from a secret admirer",
+        validMagazineText
+      )
+    ).toBe(true);
+  });
+});
